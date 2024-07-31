186745fe70aa05ea847cd02f6548671b8abed956
Abhijit Bahl <abhijit.bahl27@gmail.com>
2024-07-31 10:38:31 -0700

    Changes to test

--- src/app/home/components/sidenavigation/sidenavigation.component.ts
+++ src/app/home/components/sidenavigation/sidenavigation.component.ts
@@ -1,21 +1,40 @@
-import { Component } from '@angular/core';
-import { CategoryService } from '../../services/category.service';
+import { Component, EventEmitter, OnDestroy, Output } from '@angular/core';
 import { Category } from '../../types/category.type';
+import { CategoriesStoreItem } from '../../services/category/categories.storeItem';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidenavigation',
   templateUrl: './sidenavigation.component.html',
-  styleUrls: ['./sidenavigation.component.scss']
+  styleUrls: ['./sidenavigation.component.scss'],
 })
-export class SidenavigationComponent {
+export class SidenavigationComponent implements OnDestroy {
+  @Output()
+  subCategoryClicked: EventEmitter<number> = new EventEmitter<number>();
   categories: Category[] = [];
-  constructor(categoryService: CategoryService) {
-    this.categories = categoryService.getAllCategories();
+  subscriptions: Subscription = new Subscription();
+
+  constructor(categoryStore: CategoriesStoreItem) {
+    this.subscriptions.add(
+      categoryStore.categories$.subscribe((categories) => {
+        this.categories = categories;
+      })
+    );
   }
 
   getCategories(parentCategoryId?: number): Category[] {
-    return this.categories.filter(
-      (category) => category.parent_category_id === parentCategoryId
+    return this.categories.filter((category) =>
+      parentCategoryId
+        ? category.parent_category_id === parentCategoryId
+        : category.parent_category_id === null
     );
+  }
+
+  onSubCategoryClick(subCategory: Category): void {
+    this.subCategoryClicked.emit(subCategory.id);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
